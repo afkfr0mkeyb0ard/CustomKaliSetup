a2700a66b54de8d716a29267f760bdedb9c9f312
afkfr0mkeyb0ard <123593001+afkfr0mkeyb0ard@users.noreply.github.com>
2024-07-25 19:09:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,7 +65,7 @@ rm ffuf_2.1.0_linux_amd64.tar.gz
 # ItWasAllADream
 cd $INTERNALS_DIR
 cd ItWasAllADream
-poetry install
+poetry install || echo "[-] Failed to install ItWasAllADream"
 echo "alias activate-itwasalladream='cd \$INTERNALS_DIR/ItWasAllADream && poetry shell && itwasalladream -h'" >> ~/.zshrc
 echo "itwasalladream -u user -p password -d domain 192.168.1.0/24" >> ~/.zsh_history
 deactivate
@@ -76,7 +76,7 @@ chmod +x ldapnomnom-linux-x64
 chmod +x ldapnomnom-linux-x64-obfuscated
 
 # Manspider
-python3 -m pipx install git+https://github.com/blacklanternsecurity/MANSPIDER
+python3 -m pipx install git+https://github.com/blacklanternsecurity/MANSPIDER || echo "[-] Failed to install Manspider"
 echo "alias manspider='\$HOME/.local/share/pipx/venvs/man-spider/bin/manspider'" >> ~/.zshrc
 echo "manspider 10.10.10.0/24 -e xml -c DefaultPassword cpassword -n -u USER -p PASS -d DOMAINE" >> ~/.zsh_history
 
@@ -91,17 +91,17 @@ cd $INTERNALS_DIR
 cd mitm6
 python3 -m venv .
 source ./bin/activate
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt || echo "[-] Failed to install Mitm6"
 deactivate
 echo "alias activate-mitm6='cd \$INTERNALS_DIR/mitm6 && source ./bin/activate'" >> ~/.zshrc
 echo "python3 mitm6.py -i eth0 -d domain.local -hb [donotrespondtoFILE] [-hw <target>] [--ignore-nofqnd]" >> ~/.zsh_history
 
 # Netexec
 python3 -m pipx ensurepath
-python3 -m pipx install git+https://github.com/Pennyw0rth/NetExec
+python3 -m pipx install git+https://github.com/Pennyw0rth/NetExec || echo "[-] Failed to install Netexec"
 
 # PCredz
-sudo apt-get install libpcap-dev && pip3 install Cython && pip3 install python-libpcap
+sudo apt-get install libpcap-dev && pip3 install Cython && pip3 install python-libpcap || echo "[-] Failed to install PCredz"
 
 # Pypycatz
 pip3 install pypykatz
