5ba4ebde612c41cd5d8d18e5d632655e66508b1d
afkfr0mkeyb0ard <123593001+afkfr0mkeyb0ard@users.noreply.github.com>
2025-05-27 18:18:51 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -748,6 +748,12 @@ git clone https://github.com/lgandx/Responder.git
 echo "alias responder='$INTERNALS_DIR/Responder/Responder.py'" >> ~/.zshrc
 echo "responder -I eth0'" >> ~/.zsh_history
 
+# Ridenum
+cd $INTERNALS_DIR
+git clone https://github.com/trustedsec/ridenum.git
+echo "alias ridenum='$INTERNALS_DIR/ridenum/ridenum.py'" >> ~/.zshrc
+echo "ridenum <server_ip> <start_rid> <end_rid> <optional_username> <optional_password> <optional_password_file> <optional_username_filename>" >> ~/.zsh_history
+
 # RottenPotatoNG
 cd $INTERNALS_DIR
 git clone https://github.com/breenmachine/RottenPotatoNG.git
