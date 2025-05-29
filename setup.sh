c221b7b0c8718f4b215ae05dcb815cc5290bac29
afkfr0mkeyb0ard <123593001+afkfr0mkeyb0ard@users.noreply.github.com>
2025-05-29 19:41:24 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -348,6 +348,7 @@ cd examples
 wget https://raw.githubusercontent.com/api0cradle/impacket/a1d0cc99ff1bd4425eddc1b28add1f269ff230a6/examples/rpcchangepwd.py
 echo "alias impacket-rpcchangepwd='$INTERNALS_DIR/impacket-0.12.0/bin/python3 $INTERNALS_DIR/examples/rpcchangepwd.py'" >> ~/.zshrc
 echo "impacket-addcomputer -computer-name 'MyComputer$' -computer-pass 'Password123' -dc-host 10.10.11.222 'DOMAIN/user:password'" >> ~/.zsh_history
+echo "impacket-changepasswd \$DOMAIN/\$MACHINE_ACC:\$PASS@\$IP -newpass 'P@ssw0rd' -p rpc-samr" >> ~/.zsh_history
 echo "impacket-Get-GPPPassword 'DOMAIN'/'USER':'PASSWORD'@'DOMAIN_CONTROLLER'" >> ~/.zsh_history
 echo "impacket-GetADUsers -all -dc-ip \$IP domain.local/username" >> ~/.zsh_history
 echo "impacket-getTGT domain.local/username -hashes :<NTLM> -dc-ip <DC-IP>" >> ~/.zsh_history
